17783ab6987dae856a06b473598a4df4a44ca34b
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2017-03-06 13:58:02 +0000

    Updated graphics plots using intervene

--- Graphics/violin_plot.py
+++ Graphics/violin_plot.py
@@ -3,19 +3,20 @@
 import sys
 import argparse
 import csv
-import functools
+# import functools
 import re
 import pandas
-from collections import defaultdict
-import matplotlib.cm as cm
-import matplotlib.colors
+# from collections import defaultdict
+# import matplotlib.cm as cm
+# import matplotlib.colors
 import numpy
-import scipy.stats
+# import scipy.stats
 import matplotlib.pyplot as plt
-import matplotlib.patches
-from math import floor
+# import matplotlib.patches
+# from math import floor
 import seaborn
 from utils import parse_configuration
+import os
 
 
 def sort_values(key, dictionary):
@@ -32,17 +33,19 @@ def sort_values(key, dictionary):
 #     return data[s<m]
 
 
-def generate_plot(dataframe, args, options, nrows=1, ncols=1, max_val=10000):
+def generate_plot(star_data, tophat_data, merged_data, typ, args, options):
 
     plt.style.context("ggplot")
     print("Sorting values")
-    dataframe = dataframe.sort_values(["TPM", "tid"], ascending=True)
+    star_data = star_data.sort_values(["TPM", "tid"], ascending=True)
+    tophat_data = tophat_data.sort_values(["TPM", "tid"], ascending=True)
+    merged_data = merged_data.sort_values(["TPM", "tid"], ascending=True)
     # Remove outliers
     print("Sorted values")
 
     print("Prepared the data")
 
-    figure, axes = plt.subplots(nrows=2,
+    figure, axes = plt.subplots(nrows=3,
                                 ncols=1,
                                 figsize=(16, 10),
                                 sharex=False,
@@ -52,53 +55,40 @@ def generate_plot(dataframe, args, options, nrows=1, ncols=1, max_val=10000):
                     fontsize=20, style="italic", family="serif")
 
     # Plot complete genes
-    plt.axes(axes[0])
-    seaborn.violinplot(data=dataframe,
-                       color="turquoise",
-                       # hue="Aligner",
-                       # palette="Set2",
-                       x=6,
-                       y="TPM",
-                       cut=2,
-                       inner="box",
-                       showmeans=True,
-                       scale="width")
+    if typ == "full":
+        x_num = 6
+        line = "Number of methods capable of correctly reconstructing the transcript"
+    elif typ == "missed":
+        x_num = 1
+        line = "Number of methods which have completely missed the transcript"
+    else:
+        raise ValueError("Invalid type: {}".format(typ))
 
-    axes[0].set_xlim(-1, 9)
-    axes[0].set_xlabel("Number of methods capable of correctly reconstructing the transcript", size=18)
-    axes[0].set_ylabel("$log_{2}(TPM+1)$", size=20)
-    axes[0].xaxis.set_major_locator(plt.NullLocator())
-    # for tick in axes[0].xaxis.get_major_ticks():
-    #     tick.label.set_fontsize(0)
-    for tick in axes[0].yaxis.get_major_ticks():
-        tick.label.set_fontsize(13)
-
-    # Plot missed genes
-    plt.axes(axes[1])
-    seaborn.violinplot(data=dataframe,
-                       color="lightsage",
-                       # hue="Aligner",
-                       # palette="Set2",
-                       cut=2,
-                       x=1,
-                       y="TPM",
-                       inner="box",
-                       scale="width")
-
-    # vparts = axes[1].violinplot(nmissed, range(9), points=1000, widths=0.8,
-    #                             showextrema=False, showmeans=False, showmedians=True)
-    # for pc in vparts["bodies"]:
-    #     pc.set_facecolor("springgreen")
-    #     pc.set_edgecolor("black")
-
-    axes[1].set_xlim(-1, 9)
-    axes[1].set_xlabel("Number of methods which have completely missed the transcript", size=18)
-    axes[1].set_ylabel("$log_{2}(TPM+1)$", size=20)
-
-    for tick in axes[1].xaxis.get_major_ticks():
-        tick.label.set_fontsize(15)
-    for tick in axes[1].yaxis.get_major_ticks():
-        tick.label.set_fontsize(13)
+    for index, (method, dataframe, color) in enumerate([("STAR", star_data, "turquoise"),
+                                                        ("TopHat", tophat_data, "coral"),
+                                                        ("Either", merged_data, "slategrey")]):
+        plt.axes(axes[index])
+        seaborn.violinplot(data=dataframe,
+                           color=color,
+                           # hue="Aligner",
+                           # palette="Set2",
+                           x=x_num,
+                           y="TPM",
+                           cut=2,
+                           inner="box",
+                           showmeans=True,
+                           scale="width")
+        x_max = max(dataframe[x_num].max() + 1, dataframe[x_num].max() + 1)
+        axes[index].set_xlim(-1, x_max)
+        axes[index].set_xlabel(line + " ({})".format(method), size=18)
+        axes[index].set_ylabel("$log_{2}(TPM+1)$", size=20)
+        # axes[index].xaxis.set_major_locator(plt.NullLocator())
+        # for tick in axes[0].xaxis.get_major_ticks():
+        #     tick.label.set_fontsize(0)
+        for tick in axes[index].yaxis.get_major_ticks():
+            tick.label.set_fontsize(13)
+        for tick in axes[index].yaxis.get_major_ticks():
+                tick.label.set_fontsize(13)
 
     plt.tight_layout(pad=0.2,
                      h_pad=1,
@@ -112,7 +102,8 @@ def generate_plot(dataframe, args, options, nrows=1, ncols=1, max_val=10000):
         print("Showing the plot")
         plt.show()
     else:
-        plt.savefig(args.out,
+        out = os.path.splitext(args.out)[0] + ".{}".format(typ) + os.path.splitext(args.out)[1]
+        plt.savefig(out,
                     format=options["format"],
                     dpi=options["dpi"],
                     transparent=(not options["opaque"]))
@@ -204,45 +195,32 @@ def main():
         for num in range(1, 7):
             tophat[row["target_id"]][num] = 0
 
-    for aligner, dictionary in (("STAR", star), ("TopHat", star)):
+    for aligner, dictionary in (("STAR", star), ("TopHat", tophat)):
         for method in options["methods"]:
-            # This is the input, so no Mikado!
-            if "Mikado" in method:
-                continue
             input_file = "{}.refmap".format(
                 re.sub(".stats", "", options["methods"][method][aligner][0]))
             print(input_file)
             dictionary = analyse_refmap(input_file,
                                         dictionary)
 
-    #
-    #
-    #
-    # tids = set(values.keys())
-    # right_total = len(labels) + 2
-    # count_zero = set()
-    # for tid in tids:
-    #     if len(values[tid].keys()) == 2:
-    #         del values[tid]
-    #     elif values[tid]["TPM"] == 0:
-    #         del values[tid]
-    #         count_zero.add(tid)
-    #     elif len(values[tid].keys()) != right_total:
-    #         raise KeyError("ID {} has been found only in {}".format(tid, values[tid].keys()))
-    # tids = set.difference(tids, count_zero)
-    # if tids != set(values.keys()):
-    #     print("Removed {} TIDs due to filtering".format(len(tids) - len(set(values.keys()))))
-
-    # data = defaultdict(list)
-    data = []
+    star_data = []
+    tophat_data = []
+    merged_data = []
     for tid in star:
-        data.append([tid] + [star[tid]["TPM"]] + ["STAR"] + [star[tid][_] for _ in range(1,7)])
-    # for tid in tophat:
-    #     data.append([tid] + [tophat[tid]["TPM"]] + ["Tophat"] + [tophat[tid][_] for _ in range(1, 7)])
+        star_data.append([tid] + [star[tid]["TPM"]] + ["STAR"] +
+                    [star[tid][_] for _ in range(1,7)])
+        tophat_data.append([tid] + [tophat[tid]["TPM"]] + ["Tophat"] +
+                    [tophat[tid][_] for _ in range(1, 7)])
+        merged_data.append([tid] + [tophat[tid]["TPM"]] + ["Either"] +
+                           [star[tid][_] + tophat[tid][_] for _ in range(1, 7)])
 
-    print("Generating the final data frame")
-    data = pandas.DataFrame(data, columns=["tid", "TPM", "Aligner"] + list(range(1,7)))
-    generate_plot(data, args, options)
+    print("Generating the final data frames")
+    star_data = pandas.DataFrame(star_data, columns=["tid", "TPM", "Aligner"] + list(range(1,7)))
+    tophat_data = pandas.DataFrame(tophat_data, columns=["tid", "TPM", "Aligner"] + list(range(1,7)))
+    merged_data = pandas.DataFrame(merged_data, columns=["tid", "TPM", "Aligner"] + list(range(1,7)))
+
+    generate_plot(star_data, tophat_data, merged_data, "full", args, options)
+    generate_plot(star_data, tophat_data, merged_data, "missed", args, options)
     return
 
 main()
